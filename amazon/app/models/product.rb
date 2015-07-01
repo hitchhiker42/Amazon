df5a6dca64048f28e95945898023f5a5d533dda5
Isha <isha@Ishas-MacBook-Air.local>
2015-07-01 19:55:43 -0400

    price in dollars

--- amazon/app/models/product.rb
+++ amazon/app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ActiveRecord::Base
 
 	validates :name, :description, presence: true
-	validates :price_in_cents, {only_integer: true}
+	validates :price_in_cents, numericality: {only_integer: true}
+
+	def formatted_price
+		price_in_dollars = price_in_cents/100
+		sprintf("%.2f", price_in_dollars)
+	end
 end
