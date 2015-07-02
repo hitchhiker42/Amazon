ba7d11482e1eb0f8a45fabc3805bafb09aa62f41
Isha <isha@Ishas-MacBook-Air.local>
2015-07-02 01:01:31 -0400

    Adding authorization

--- amazon/config/routes.rb
+++ amazon/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get 'sessions/new'
 
   get 'sessions/create'
@@ -7,7 +8,10 @@ Rails.application.routes.draw do
 
   # root 'welcome#index'
 
-  resources :products
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
+  
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
