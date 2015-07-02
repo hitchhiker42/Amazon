3f08e54c960b0447c8d2463a915f01f38deeec3b
Isha <isha@Ishas-MacBook-Air.local>
2015-07-01 22:34:20 -0400

    Creating log in functionality

--- amazon/config/routes.rb
+++ amazon/config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # root 'welcome#index'
 
   resources :products
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
 end
 
