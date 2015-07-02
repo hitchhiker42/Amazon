e6cc438c7baaf8583001b6dbf296ec530854bb2e
Isha <isha@Ishas-MacBook-Air.local>
2015-07-01 22:22:14 -0400

    Creating a user model, controller, view

--- amazon/config/routes.rb
+++ amazon/config/routes.rb
@@ -1,17 +1,29 @@
 Rails.application.routes.draw do
-  get 'products/index'
+  # root 'welcome#index'
 
-  get 'products/show'
+  resources :products
+  resources :users, only: [:new, :create]
 
-  get 'products/new'
+end
 
-  get 'products/edit'
+
+  # get 'users/new'
+
+  # get 'users/create'
+
+  # get 'products/index'
+
+  # get 'products/show'
+
+  # get 'products/new'
+
+  # get 'products/edit'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -61,4 +73,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
--- amazon/db/seeds.rb
+++ amazon/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user1 = User.new(email: 'yaya', password: 'pass', password_confirmation: 'pass')
